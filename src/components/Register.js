2c4568f19421f1991a512d8764be9b0d19f89db0
Facu J <facu.jalil@gmail.com>
2023-05-01 16:18:14 -0300

    update 24

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,23 @@
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import Content from "./Content";
+import { useNavigate } from "react-router-dom";
 
 export const Register = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const navigate = useNavigate()
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [requestCode, setRequestCode] = useState(false)
 
     const [errorRegisterPassword, setErrorRegisterPassword] = useState(false)
+    const [errorValidateCode, setErrorValidateCode] = useState(false)
 
     const registerUser = async (email, contraseña) => {
-        let response = await fetch('https://rbrain.onrender.com/signup', {
+        let response = await fetch('https://rbrain.onrender.com/send-code-validation', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -21,7 +25,8 @@ export const Register = () => {
             body: JSON.stringify({ 'email': email, 'password': contraseña })
         })
         let data = await response.json()
-        if (response.status !== 201) {
+        if (response.status === 200) {
+            console.log("OK")
             setRequestCode(true)
         } else {
             setErrorRegisterPassword('invalidPassword')
@@ -46,8 +51,21 @@ export const Register = () => {
         }
     }
 
-    const validateCode = (value) => {
-        console.log(email, password, value.code)
+    const validateCode = async (value) => {
+        let response = await fetch('https://rbrain.onrender.com/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'email': email, 'password': password, 'verification_code': value.code })
+        })
+        let data = await response.json()
+        if (response.status === 201) {
+            navigate('/login')
+            setRequestCode(true)
+        } else {
+            setErrorValidateCode(true)
+        }
     }
 
     return (
@@ -60,9 +78,10 @@ export const Register = () => {
                 requestCode ?
                     <form className="form-validate" onSubmit={handleSubmit(validateCode)}>
                         <label htmlFor="validate">Te hemos enviado un código de verificación al email</label>
-                        <input type="number" id="validate" placeholder="Ingrese el código" {...register('code', {
+                        <input className={errorValidateCode ? "error" : null} type="text" id="validate" placeholder="Ingrese el código" {...register('code', {
                             required: true
                         })} />
+                        {errorValidateCode ? <div className="container-error"><p>El código ingresado no es correcto</p></div> : null}
                     </form>
                     :
                     <form onSubmit={handleSubmit(registerValidate)}>
