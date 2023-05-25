e6973e5d586c5d0e5f41bd9e647262aff61f94ff
FakeCreator01 <102307965+FakeCreator01@users.noreply.github.com>
2023-05-25 16:35:30 -0300

    timeout auth tokens 4 minutes | FRANCO

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,52 +1,52 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect } from 'react';
 import jwt_decode from 'jwt-decode';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
+  const [errorLoginApi, setErrorLoginApi] = useState(false);
+  const [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null
+  );
+  const [user, setUser] = useState(() =>
+    localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null
+  );
+  const [loading, setLoading] = useState(true);
 
-    const [errorLoginApi, setErrorLoginApi] = useState(false)
+  const navigate = useNavigate();
 
-    const [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null);
-    const [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null);
-    const [loading, setLoading] = useState(true)
+  const loginUser = async (valor) => {
+    const response = await fetch('https://rbrain.onrender.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: valor.loginEmail, password: valor.loginPassword }),
+    });
 
-    const navigate = useNavigate()
+    const data = await response.json();
 
-    const loginUser = async (valor) => {
-
-        const response = await fetch('https://rbrain.onrender.com/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 'email': valor.loginEmail, 'password': valor.loginPassword })
-        })
-
-        const data = await response.json()
-
-        if (response.status === 200) {
-            setAuthTokens(data)
-            setUser(jwt_decode(data.access_token))
-            localStorage.setItem('authTokens', JSON.stringify(data))
-            navigate('/profile')
-            setErrorLoginApi(false)
-
-        } else {
-            setErrorLoginApi(true)
-        }
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access_token));
+      localStorage.setItem('authTokens', JSON.stringify(data));
+      navigate('/profile');
+      setErrorLoginApi(false);
+    } else {
+      setErrorLoginApi(true);
     }
+  };
 
-    const logoutUser = () => {
-        setAuthTokens(null)
-        setUser(null)
-        localStorage.removeItem('authTokens')
-    }
+  const logoutUser = () => {
+    setAuthTokens(null);
+    setUser(null);
+    localStorage.removeItem('authTokens');
+  };
 
-    const updateToken = async () => {
+  const updateToken = async () => {
     if (authTokens && authTokens.refresh_token) {
       const response = await fetch('https://rbrain.onrender.com/refresh', {
         method: 'POST',
@@ -69,31 +69,39 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-    useEffect(() => {
+  useEffect(() => {
+    let timeout;
 
-        if (loading) {
-            updateToken()
-            setLoading(false);
-        }
+    const fourMinutes = 1000 * 60 * 4;
 
-        const fourMinutes = 1000 * 600 * 4
-        const interval = setInterval(() => {
-            if (authTokens) {
-                updateToken();
-            }
-        }, fourMinutes);
-        
-        return () => clearInterval(interval)
-    }, [authTokens, loading])
+    const updateTokenWithTimeout = async () => {
+      await updateToken();
+      timeout = setTimeout(updateTokenWithTimeout, fourMinutes);
+    };
 
-    return (
-        <AuthContext.Provider value={{ user, authTokens, loginUser, logoutUser, errorLoginApi }} >
-            {loading ? null : children}
-            {/*{useEffect(() => {
-            if(loading){
-                setLoading(false)
-            }
-        }, [loading])}*/}
-        </AuthContext.Provider>
-    )
-}
+    const startTimeout = () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(updateTokenWithTimeout, fourMinutes);
+    };
+
+    if (loading) {
+      updateTokenWithTimeout();
+      setLoading(false);
+    }
+
+    document.addEventListener('mousemove', startTimeout);
+    document.addEventListener('keypress', startTimeout);
+
+    return () => {
+      clearTimeout(timeout);
+      document.removeEventListener('mousemove', startTimeout);
+      document.removeEventListener('keypress', startTimeout);
+    };
+  }, [authTokens, loading]);
+
+  return (
+    <AuthContext.Provider value={{ user, authTokens, loginUser, logoutUser, errorLoginApi }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
