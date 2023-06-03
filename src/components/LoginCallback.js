285c4d1a3bab1e8f5965dcdfed97123fac27714d
FakeCreator01 <102307965+FakeCreator01@users.noreply.github.com>
2023-06-03 00:03:19 -0300

    Update LoginCallback.js | FRANCO

--- src/components/LoginCallback.js
+++ src/components/LoginCallback.js
@@ -15,7 +15,7 @@ export default function LoginCallback() {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ code: encodedCode }),
+          body: JSON.stringify({ code: code }),
         });
 
                 if (response.ok) {
